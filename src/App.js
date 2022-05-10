a52164a4ed7ab3f5f41c05fb300597fa6bdaf0dd
joao <joao.otavio00@hotmail.com>
2022-05-10 20:43:49 -0300

    layout

--- src/App.js
+++ src/App.js
@@ -2,25 +2,46 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <div className='filtros'>
+          <h3>Filtros</h3>
+
+          <label>Valor mínimo:</label>
+          <input type="number"/>
+
+          <label>Valor máximo:</label>
+          <input type="number"/>
+
+          <label>Buscar por nome:</label>
+          <input type="text"/>
+        </div>
+
+        <div className='cabecalho'>
+          <h3>Quantidade de produtos:</h3>
+
+            <div className='ordem'>
+              <p>Ordenação:</p>
+              <select>
+                <option>Crescente</option>
+                <option>Decrescente</option>
+              </select>
+
+            </div>
+        </div>
+
+
+        <div className='carrinho'>
+          <h3>Carrinho:</h3>
+          <p>Valor total: R$0,00</p>
+        </div>
+      </div>
+      
+    );
+  }
 }
 
 export default App;
